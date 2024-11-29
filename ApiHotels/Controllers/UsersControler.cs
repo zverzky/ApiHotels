7c028b8c7eb6a451747d9ae235bb183c2fcf25ec
Коляшаа Колесников <kolnik230501@mail.ru>
2024-11-29 14:30:43 +0300

    перенос auth в блл

--- ApiHotels/Controllers/UsersControler.cs
+++ ApiHotels/Controllers/UsersControler.cs
@@ -28,25 +28,26 @@ public class UsersController : Controller
     [HttpPost("login")]
     public IActionResult Login([FromBody] UserLoginModel user)
     {
-        if (user is null)
-        {
-            return BadRequest("Invalid client request");
-        }
-        if (user.Email == "johndoe@mail.ru" && user.Password == "def@123")
-        {
-            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@345"));
-            var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
-            var tokeOptions = new JwtSecurityToken(
-                issuer: "https://localhost:5001",
-                audience: "https://localhost:5001",
-                claims: new List<Claim>(),
-                expires: DateTime.Now.AddMinutes(5),
-                signingCredentials: signinCredentials
-            );
-            var tokenString = new JwtSecurityTokenHandler().WriteToken(tokeOptions);
-            return Ok(new AuthenticatedResponse { Token = tokenString });
-        }
-        return Unauthorized();
+        //if (user is null)
+        //{
+        //    return BadRequest("Invalid client request");
+        //}
+        //if (user.Email == "johndoe@mail.ru" && user.Password == "def@123")
+        //{
+        //    var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@345"));
+        //    var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+        //    var tokeOptions = new JwtSecurityToken(
+        //        issuer: "https://localhost:5001",
+        //        audience: "https://localhost:5001",
+        //        claims: new List<Claim>(),
+        //        expires: DateTime.Now.AddMinutes(5),
+        //        signingCredentials: signinCredentials
+        //    );
+        //    var tokenString = new JwtSecurityTokenHandler().WriteToken(tokeOptions);
+        //    return Ok(new AuthenticatedResponse { Token = tokenString });
+        //}
+        //return Unauthorized();
+        return Ok(user);
     }
     [HttpGet("{id}")]
     public ActionResult<UserWithResponse> GetUserById([FromRoute] Guid id)
